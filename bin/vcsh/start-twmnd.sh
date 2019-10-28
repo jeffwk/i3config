3d1b6fc3c8eaaaa16be65c85387c33fadaf1c5ab
Jeff Workman <jeff.workman@gmail.com>
2019-10-28 00:44:08 -0400

    New theme: Input Mono, i3-gaps rounded w/ bright desktop, colors

--- bin/vcsh/start-twmnd.sh
+++ bin/vcsh/start-twmnd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-killall twmnd 2> /dev/null ; sleep 0.25
-killall -9 twmnd 2> /dev/null ; sleep 0.25
+killall twmnd 2> /dev/null ; sleep 0.1
+killall -9 twmnd 2> /dev/null ; sleep 0.1
 twmnd & sleep 0.5
 twmnc -t "twmnd" -c "started" -d 2000
