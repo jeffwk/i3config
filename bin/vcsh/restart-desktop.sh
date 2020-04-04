012c88a90eb67e1d07f6850480aac4d2270e8a35
Jeff Workman <jeff.workman@gmail.com>
2020-04-04 16:13:58 -0400

    deadd notify, fixes, desktop updates

---
+++ bin/vcsh/restart-desktop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pkill picom
+pkill stalonetray
+sleep 0.3 && picom -b --experimental-backends &
+sleep 0.05 && stalonetray &
+sleep 0.05 && restart-notify.sh &
+sleep 0.05 && i3-msg 'focus tiling'
